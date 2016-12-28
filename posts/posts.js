707692488c1a479af8cbbe40bd80aa7572259366
RadikalJin <jdhsoc@gmail.com>
2016-12-28 23:10:41 +0000

    Add new post

--- posts/posts.js
+++ posts/posts.js
@@ -3,7 +3,7 @@ var postsModule = angular.module("entriesApp", []);
 postsModule.controller("myCtrl", function($scope, $http) {
     $scope.clickedMoreButton = function() {
         sessionStorage.setItem('postToEdit', JSON.stringify(this.post));
-        window.location = '/editor';
+        window.location = location.hostname + '/editor';
         //TODO Change this url
     }
 
@@ -12,21 +12,12 @@ postsModule.controller("myCtrl", function($scope, $http) {
         var url = $scope.postURL;
         $http.post(url).then(function(response) {
             var posts = JSON.parse(response.data.message);
-            posts = formatDates(posts);
-            $scope.posts = posts;
+            $.getScript('scripts/dateUtils.js', function() {
+                posts = addFormattedDateToPosts(posts);
+                $scope.posts = posts;
+            });
         });
     };
-    function formatDates(data) {
-        var m_names = new Array("January", "February", "March",
-            "April", "May", "June", "July", "August", "September",
-            "October", "November", "December");
-        for (var i = 0; i < data.length; i++) {
-            var currentPost = data[i];
-            var date = new Date(+parseInt(currentPost.createdDate, 10));
-            currentPost.formattedDate = m_names[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
-        }
-        return data;
-    }
 });
 
 postsModule.filter("trust", ['$sce', function($sce) {
