b04093c60a388f5ef266f5ef8f2914c1877db1dd
RadikalJin <jdhsoc@gmail.com>
2016-12-29 01:03:07 +0000

    Added LocationUtils

---
+++ scripts/locationUtils.js
@@ -0,0 +1,21 @@
+function getURLByBlogId(blogId) {
+    var result = '';
+    switch(blogId) {
+        case '1':
+            result = 'fitness';
+            break;
+        default:
+            result = undefined;
+    }
+    return result;
+}
+
+function getSendToURLByPost(post) {
+    var extension;
+    if (post.id) {
+        extension = 'updatePost';
+    } else {
+        extension = 'createPost';
+    }
+    return 'http://josephhoare.com:8090/' + extension;
+}
