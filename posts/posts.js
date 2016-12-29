ab8befdbf39374d936704cbfb6d9847827b3e5c8
RadikalJin <jdhsoc@gmail.com>
2016-12-29 01:10:34 +0000

    Added loadScript for date utils

--- posts/posts.js
+++ posts/posts.js
@@ -3,8 +3,7 @@ var postsModule = angular.module("entriesApp", []);
 postsModule.controller("myCtrl", function($scope, $http) {
     $scope.clickedMoreButton = function() {
         sessionStorage.setItem('postToEdit', JSON.stringify(this.post));
-        window.location = location.hostname + '/editor';
-        //TODO Change this url
+        window.location = '../editor';
     }
 
     $scope.$watch("postURL", assignURL, true);
@@ -12,9 +11,8 @@ postsModule.controller("myCtrl", function($scope, $http) {
         var url = $scope.postURL;
         $http.post(url).then(function(response) {
             var posts = JSON.parse(response.data.message);
-            $.getScript('../scripts/dateUtils.js', function() {
-                posts = addFormattedDateToPosts(posts);
-                $scope.posts = posts;
+            loadScript("http://josephhoare.com/scripts/dateUtils.js", function() {
+                $scope.posts = addFormattedDateToPosts(posts);
             });
         });
     };
@@ -30,5 +28,20 @@ angular.element(document).ready(function() {
     angular.bootstrap(document.getElementById("App1"), ["entriesApp"]);
 });
 
+function loadScript(url, callback) {
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
 
 
